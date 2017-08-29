0a0de0373c7dd60d0cd854bacaecd5d5880b6624
ChuckBTaylor <WLScripps@gmail.com>
2017-08-29 14:28:02 -0400

    Pull this

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,48 +3,43 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_array = JSON.parse(all_characters)["results"] #returns array of characters
-  character_array.find do |person|
-    character == person["name"]
-  end["films"]
-
-
-
-
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  counter =   1
+  found = nil
+  loop do
+    all_characters = RestClient.get("http://www.swapi.co/api/people/?page=#{counter}")
+    character_array = JSON.parse(all_characters)["results"] #returns array of characters
+    found = character_array.find do |person|
+      character == person["name"]
+    end #Returns array of URLs
+    counter += 1
+    break if found != nil
+  end
+  found != nil ? return found["films"] : return nil
 end
 
 def parse_character_movies(film_array)
   neat_films = {}
   film_array.each do |url|
     movie = RestClient.get("#{url}")
-    neat_films[url[-2] = JSON.parse(movie)["title"])
+    neat_films[url[-2]] = JSON.parse(movie)["title"]
+  end # Returns hash of films with k/v pair of film# / title
+  neat_films.map do |film_num, title|   # {film_num => title, film_num => title, ...}
+    {film_num => title}
   end
-  neat_films
-  binding.pry
-end
+end#Returns [{film_num => title},{film_num=>title}]
 
 def print_movies(movie_array)
-  counter = 1
   movie_array.each do |movie|
-    puts "#{counter} #{movie}"
-    counter += 1
+    movie.each do |num, title|
+      puts "Star Wars episode #{num}: #{title}"
+    end
   end
 end
 
 def show_character_movies(character)
-  film_array = get_character_movies_from_api(character)
-  neat_films = parse_character_movies(film_array)
+  film_array = get_character_movies_from_api(character) #Returns array of URLs
+  film_array neat_films = parse_character_movies(film_array) #Returns Array of hashes
+                                                  #e.g.[{1=>"A New Hope"},2=>"Empire Strikes Back"}]
   print_movies(neat_films)
 end
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -4,5 +4,16 @@ end
 
 def get_character_from_user
   puts "please enter a character"
-  gets.chomp
+  choice = gets.chomp
+  capitalize_name(choice)
+end
+
+def capitalize_name(name)
+  if name.index(/[0123456789-]/)
+    return name.upcase
+  else
+    name.split.map do |word|
+      word.capitalize
+    end.join(" ")
+  end
 end
